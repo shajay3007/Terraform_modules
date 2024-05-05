9e10a507353bdfbb0e01d1ef0ee41891eb450994
shajay <shajay3007@gmail.com>
2024-05-05 23:16:23 +0530

    module code

---
+++ create_ec2/module/ec2_instance/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  subnet_id = var.subnet_id_value
+  associate_public_ip_address = true
+}
---
+++ create_ec2/module/ec2_instance/variable.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+  
+}
+variable "instance_type_value" {
+  
+}
+variable "subnet_id_value" {
+  
+}
